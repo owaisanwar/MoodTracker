c1ac469d2eb209795dadc6abcf2c2f063e99424e
owaisanwar <owaisanwar396@gmail.com>
2022-10-29 08:47:51 +0500

    fonts added to project

--- src/screens/BottomTabs.navigator.tsx
+++ src/screens/BottomTabs.navigator.tsx
@@ -3,15 +3,40 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { History } from "./History.Screen";
 import { Home } from "./Home.Screen";
 import { Analytic } from "./Analytic.Screen";
+import { Text } from "react-native";
+import { AnalyticIcon, HomeIcon, ListIcon } from "../components/Icons";
+import { theme } from "../theme";
 
 const BottomTabs = createBottomTabNavigator();
 
 export const BottomTabsNavigator: React.FC = () => {
     return (
-        <BottomTabs.Navigator>
-            <BottomTabs.Screen name="Home" component={Home} />
-            <BottomTabs.Screen name="History" component={History} />
-            <BottomTabs.Screen name="Analytic" component={Analytic} />
+        <BottomTabs.Navigator screenOptions={
+            ({ route }) => ({
+                tabBarActiveTintColor: theme.colorBlue,
+                tabBarInactiveTintColor: theme.colorGrey,
+                tabBarShowLabel: false,
+                tabBarIcon: ({ color, size }) => {
+                    switch (route.name) {
+                        case 'Home':
+                            return <HomeIcon size={size} color={color} />
+                        case 'History':
+                            return <ListIcon size={size} color={color} />
+                        case 'Analytic':
+                            return <AnalyticIcon size={size} color={color} />
+                    }
+                }
+            })
+        }>
+            <BottomTabs.Screen name="Home" component={Home} options={{
+                title: 'Today\'s Mood'
+            }} />
+            <BottomTabs.Screen name="History" component={History} options={{
+                title: "Past Moods"
+            }} />
+            <BottomTabs.Screen name="Analytic" component={Analytic} options={{
+                title: "Fancy Graph"
+            }} />
         </BottomTabs.Navigator>
     )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,11 @@ export const theme = {
     colorPurple: '#454C73',
     colorWhite: '#fff',
     colorLavender: '#87677B',
+    colorBlue: '#1D84B5',
+    colorGrey: '#8E9AAF',
+    fontFamilyBold: 'Kalam-Bold',
+    fontFamilyRegular: 'Kalam-Regular',
+    fontFamilyLight: 'Kalam-Light',
     // spaces
     space10: 10,
     space12: 12,
